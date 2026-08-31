f54b6a5e0e87780c6c11940a621e66b78d135581
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2026-08-31 23:31:15 +0800

    Updates

---
+++ assets/swimlanesDiagram-VR7AAH4N-CjnsPkRU.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-HODETNUW-ClkRmykb.js";import{_ as a}from"./index-Dl1_Qr04.js";import"./chunk-5VM5RSS4-Cu6AwArX.js";import"./chunk-XXDRQBXY-BiCNJQ7r.js";import"./chunk-POPQ4Y6H-0ZxvZFpP.js";import"./chunk-F27PBJKO-KBB4nkYW.js";import"./channel-B7y4Hirr.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
